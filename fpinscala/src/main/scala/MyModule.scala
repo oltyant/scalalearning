d003b879b9ea93adcd8e524c74184615cf4d1da6
tamas.oltyan <tamas.oltyan@users.noreply.github.com>
2015-03-12 07:36:17 +0100

    [++] Add MyModule.{factorial, fib} tests (Specs2)

--- fpinscala/src/main/scala/MyModule.scala
+++ fpinscala/src/main/scala/MyModule.scala
@@ -6,9 +6,9 @@
 object MyModule {
   def abs(n: Int): Int = if (n < 0) -n else n
 
-  def factorial(n: Int): Int = {
+  def factorial(n: Int): BigInt = {
     @annotation.tailrec
-    def go(n: Int, acc: Int): Int = {
+    def go(n: Int, acc: BigInt): BigInt = {
       if (n <= 0) acc
       else go(n - 1, n * acc)
     }
@@ -22,15 +22,15 @@ object MyModule {
    */
   def fib(n: Int): Int = {
     @annotation.tailrec
-    def go(act: Int, acc2: Int, acc1: Int): Int = n match {
-      case 0 => acc2
+    def go(act: Int, acc2: Int, acc1: Int): Int = act match {
+      case num: Int if num <= 0  => acc2
       case _ =>  go(act - 1, acc1, acc2 + acc1)
     }
 
     go(n, 0, 1)
   }
 
-  private def formatFunction(n: Int, name: String)(f: Int => Int): String = s"The $name value of $n is ${f(n)}"
+  private def formatFunction(n: Int, name: String)(f: Int => BigInt): String = s"The $name value of $n is ${f(n)}"
 
   def main(args: String*) = println(formatFunction(-42, "factorial")(factorial))
 }
--- fpinscala/src/test/scala/TestMyModule.scala
+++ fpinscala/src/test/scala/TestMyModule.scala
@@ -1,5 +1,8 @@
 import org.specs2.mutable.Specification
 
+import scala.util.Random
+import MyModule._
+
 /**
  * Created by oltyan on 3/11/15.
  *
@@ -9,21 +12,55 @@ object TestMyModule extends Specification {
   "The abs method" should {
     "result the input number wihout sign when the input number is negative" in {
       val negativeNumber = -2
-      val module = MyModule
-      val result = module.abs(negativeNumber)
+      val result = abs(negativeNumber)
       result mustEqual 2
     }
     "result the input number when the input number is positive" in {
       val positiveNumber = 2
-      val module = MyModule
-      val result = module.abs(positiveNumber)
+      val result = abs(positiveNumber)
       result mustEqual positiveNumber
     }
     "result the input number when the input number is zero" in {
       val zero = 0
-      val module = MyModule
-      val result = module.abs(zero)
+      val result = abs(zero)
       result mustEqual zero
+    }
+  }
+
+  "The factorial method" should {
+    "Give back the proper one value when the input number is less than or equal one" in {
+      val inputs = List(1, 0, new Random().nextInt(10) * -1)
+      inputs.map(factorial).forall(_ mustEqual 1)
+    }
+    "Give back the proper factorial when the input number is greater than one" in {
+      //ugly imperative definition of factorial computing
+      //If it does not give back the same value that the tail recursive factorial definition in MyModule
+      //then one of the definitions will be wrong
+      def fact(n: Int): BigInt = {
+        var f: BigInt = 1
+        (1 to n).foreach(f *= _)
+        f
+      }
+      val random = new Random().nextInt(100)
+      val inputs = List(2, random).map(factorial)
+      inputs.head mustEqual fact(2)
+      inputs.last mustEqual fact(random)
+    }
+  }
+
+  "The fib method" should {
+    "Give back zero when the input number is less than or equal 0" in {
+      val random = -(new Random().nextInt(100))
+      fib(0) mustEqual 0
+      fib(random) mustEqual 0
+    }
+    "Give back 1 when the input is either 1 or 2" in {
+      fib(1) mustEqual 1
+      fib(2) mustEqual 1
+    }
+    "Give back the proper number when the input is greater than 2" in {
+      fib(3) mustEqual 2
+      fib(30) mustEqual 832040
     }
   }
 }
